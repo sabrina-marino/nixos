0fb227942bd98aa687305601ff155d517e76e6e2
Sabbyblue <152991379+sabbyblue@users.noreply.github.com>
2024-11-24 17:46:24 -0300

    SYNTAX

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs = { self, nixpkgs, nix-flatpak }: {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"
+      system = "x86_64-linux";
       modules = [
         nix-flatpak.nixosModules.nix-flatpak
         ./configuration.nix
